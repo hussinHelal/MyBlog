14dd181de59269efa81f0436deaeb47bdf0c1fcd
husseinHelal <117811379+hussinHelal@users.noreply.github.com>
2026-04-18 00:09:14 +0200

    Add trustProxies middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -26,6 +26,12 @@ return Application::configure(basePath: dirname(__DIR__))
             'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
             'role' => EnsureUserHasRole::class,
         ]);
+           $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
+        \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB
+    );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
