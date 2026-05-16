873e983214ab896b747a03092d2c91e20e998e1a
hussinHelal <hussinhelal202@gmail.com>
2026-05-16 18:33:20 +0300

    nthng

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
             'role' => EnsureUserHasRole::class,
         ]);
-           $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
+        $middleware->trustProxies(at: '*', headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR |
         \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST |
         \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT |
         \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO |
